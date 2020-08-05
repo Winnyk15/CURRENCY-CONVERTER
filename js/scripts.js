cb795c0327fe89ef8bcb65a8a52c587e555ad5c6
tonyishangu <tonyishangu@gmail.com>
2020-08-05 22:48:19 +0300

    add changes

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//function convertCurrency(){
+  //  var from = document.getElementById("from").value;
+  //  var to = document.getElementById("to").value;
+  //  var xmlhttp = new XMLHttpRequest();
+  //  var url = "https://api.exchangeratesapi.io/latest/" + from + "," + to;
+  //  xmlhttp.open("GET", url, true);
+  //  xmlhttp.send();
+  //  xmlhttp.onreadystatechange = function(){
+    //    if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
+      //      var result = xmlhttp.responseText;
+        //    alert(result);
+        //    var jsResult = JSON.parse(result);
+        //    var oneUnit = jsResult.rates[from];
+        //    var amt = document.getElementById("fromAmount").value;
+        //    document.getElementById("toAmount").value = (oneUnit*amt).toFixed(2);
+      //  };
+   // };
+//};
+
+//0ef91cfec079b086c645
